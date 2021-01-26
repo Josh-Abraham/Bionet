dbbfd32a3df21fbd96364d9def4a59c3f01e5022
Josh Abraham <35229737+Josh-Abraham@users.noreply.github.com>
2021-01-26 00:25:00 -0500

    creating updated ui

---
+++ UI_2/app/src/main/java/com/application/projecttbh/LoginPage.java
@@ -0,0 +1,72 @@
+package com.application.projecttbh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
+public class LoginPage extends Activity {
+    private Button loginBtn;
+    private TextView empIdTextview;
+    private TextView pswdTextField;
+    private TextView loginError;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        // Set object instances for UI items
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        empIdTextview = (TextView) findViewById(R.id.empIdTextview);
+        pswdTextField = (TextView) findViewById(R.id.pswdTextField);
+        loginError = (TextView) findViewById(R.id.loginError);
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String username = empIdTextview.getText().toString();
+                String password = pswdTextField.getText().toString();
+                validateLogin(username, password);
+            }
+        });
+
+    }
+    private void validateLogin(String username, String password) {
+        String url = "https://w57jsrc6wj.execute-api.us-east-2.amazonaws.com/test/getuserinfo?userId=1";
+        // Create the HTTP Request Queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string
+                        Toast.makeText(getApplicationContext(), String.format("%s\n", response), Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String test = "ERROR Loading Resource";
+                Toast.makeText(getApplicationContext(), String.format("%s\n", test), Toast.LENGTH_LONG).show();
+            }
+        });
+//		queue.add(jsonObjectRequest);
+//		loginError.setVisibility(View.VISIBLE);
+      //  Intent intent = new Intent(this, IOIOConnector.class); // Call a secondary view
+       // startActivity(intent);
+    }
+}
